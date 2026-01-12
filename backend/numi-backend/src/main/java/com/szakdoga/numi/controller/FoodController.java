9eed7b849b902031985300aa14e677044d8ec63b
KovacsDaniel0529 <99874166+KovacsDaniel0529@users.noreply.github.com>
2026-01-12 23:53:47 +0100

    Update FoodController.java

--- backend/numi-backend/src/main/java/com/szakdoga/numi/controller/FoodController.java
+++ backend/numi-backend/src/main/java/com/szakdoga/numi/controller/FoodController.java
@@ -6,25 +6,25 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController // Jelzi, hogy ez egy API végpont
-@RequestMapping("/api/foods") // Ezen a címen lesz elérhető: localhost:8080/api/foods
-@CrossOrigin(origins = "*") // FONTOS! Ez engedi majd a React-nak a hozzáférést
+@RestController 
+@RequestMapping("/api/foods") 
+@CrossOrigin(origins = "*")
 public class FoodController {
 
     private final FoodRepository foodRepository;
 
-    // Itt injektáljuk be a Repository-t (Constructor Injection)
+    
     public FoodController(FoodRepository foodRepository) {
         this.foodRepository = foodRepository;
     }
 
-    // 1. Összes étel lekérése (GET kérés)
+   
     @GetMapping
     public List<Food> getAllFoods() {
         return foodRepository.findAll();
     }
 
-    // 2. Új étel hozzáadása (POST kérés) - Ezt majd később teszteljük
+  
     @PostMapping
     public Food createFood(@RequestBody Food food) {
         return foodRepository.save(food);
